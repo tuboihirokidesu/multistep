36dbd6177fc4a7e8efa376ef9fc7627973d42db8
tuboihiroki <tuboi.hiroki@icloud.com>
2021-01-07 12:18:23 +0900

    prettier & eslint 環境構築

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { FC } from 'react';
 import './App.css';
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: FC = () => {
+  return <div></div>;
+};
 
 export default App;
